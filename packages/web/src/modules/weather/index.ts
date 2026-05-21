36f62fb55a5494defed74319a0d6106a324d891c
梁志敏 <2728460254@qq.com>
2026-05-21 12:44:33 +0800

    重构: 移除天气视觉特效模块

--- packages/web/src/modules/weather/index.ts
+++
@@ -1,4 +0,0 @@
-export { weatherRuntime } from './weatherRuntime'
-export { weatherParticles } from './WeatherParticles'
-export { default as WeatherOverlay } from './WeatherOverlay.vue'
-export { default as WeatherSettings } from './WeatherSettings.vue'
